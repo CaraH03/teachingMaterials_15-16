d9996a9bf7067189dc96e419c6f966418b3a6276
Mike Crabb <m.j.crabb@rgu.ac.uk>
2015-11-12 15:58:57 +0000

    Fixes

--- CM3028/phpLab1/Activity4-SMS3.php
+++ CM3028/phpLab1/Activity4-SMS3.php
@@ -17,14 +17,17 @@
 
 $myArray = array("specs", "mugs", "sausage rolls");
 
+echo "<h1>Original Array</h1>";
 printmyArray($myArray);
 
 $myArray[1] = "hugs"; // modifies position 1 (re)
 
+echo "<h1>Swap in Hugs</h1>";
 printmyArray($myArray);
 
-unset($array[2]); // removes the array in position 2
+unset($myArray[2]); // removes the array in position 2
 
+echo "<h1>Take out Sausage Rolls</h1>";
 printmyArray($myArray);
 
 
