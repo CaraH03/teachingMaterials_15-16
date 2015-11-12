5ff8267bddfbda7bf16311e24c840416ad34579e
Mike Crabb <m.j.crabb@rgu.ac.uk>
2015-11-12 16:00:55 +0000

    Fixes

--- CM3028/phpLab1/Activity6-SMS5.php
+++ CM3028/phpLab1/Activity6-SMS5.php
@@ -31,15 +31,15 @@ while(($spec_stock > 0) && ($mug_stock > 0) && ($sausageroll_stock > 0)) {
     switch ($todaysGood) {
         case 0:
             $spec_stock--;
-            echo "On day " . $monthDate . " Specs are available";
+            echo "On day " . $monthDate . " Specs are available<br>";
             break;
         case 1:
             $mug_stock--;
-            echo "On day " . $monthDate . " Mugs are available";
+            echo "On day " . $monthDate . " Mugs are available<br>";
             break;
         case 2:
             $sausageroll_stock--;
-            echo "On day " . $monthDate . " Sausage Rolls are available";
+            echo "On day " . $monthDate . " Sausage Rolls are available<br>";
             break;
     }
     $monthDate++; //puts the month forward by one day
