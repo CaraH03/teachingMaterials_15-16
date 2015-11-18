6dc26bf107b51375c27a54f3af4179a04875ac0d
Mike Crabb <m.j.crabb@rgu.ac.uk>
2015-11-18 10:54:03 +0000

    CM3028 Bits

---
+++ CM3028/phpLab2/finalTask/deletesuperhero.php
@@ -0,0 +1,16 @@
+<?php
+$superheroID = $GET_["id"];
+
+$db = new mysqli(
+    "eu-cdbr-azure-west-c.cloudapp.net",
+    "b0c2ff384f05bb",
+    "82b4d3a0",
+    "mjc7778db"
+);
+
+// Delete
+$sql = "DELETE FROM superherotask WHERE index='$superheroID'";
+$result = $db->query($sql);
+
+header('Location: displaySuperheros.php');
+?>
