2433454394684dc590974fefab744a499c5bf1b7
Mike Crabb <m.j.crabb@rgu.ac.uk>
2015-11-18 11:23:19 +0000

    asdf

--- CM3028/phpLab3/loggedIn.php
+++ CM3028/phpLab3/loggedIn.php
@@ -9,11 +9,13 @@ $accesslevel = $_COOKIE['access_level'];
 
 displayAccessLevel($accesslevel);
 
-function displayAccessLevelInformation($accessLevel) {
+function displayAccessLevelInformation($accessLevel)
+{
     if ($accessLevel == "standarduser") {
         echo "<p>You are currently logged in as a standard user</p>";
-    }
-    elseif ($accessLevel == "root") {
+    } elseif ($accessLevel == "root") {
         echo "<p>You are currently logged in as a root user</p>";
         echo "<p>You now have access to additional administrative features</p>";
     }
+}
+    ?>
