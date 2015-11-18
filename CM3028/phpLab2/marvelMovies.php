7afc80faf58dab1d394b59960624745bf5f76d52
Mike Crabb <m.j.crabb@rgu.ac.uk>
2015-11-18 09:49:10 +0000

    CM3028 Bits

---
+++ CM3028/phpLab2/marvelMovies.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelcrabb
+ * Date: 18/11/2015
+ * Time: 09:43
+ */
+
+$db = new mysqli(
+    "eu-cdbr-azure-west-c.cloudapp.net",
+    "b0c2ff384f05bb",
+    "82b4d3a0",
+    "mjc7778db"
+);
+
+// test if connection was established, and print any errors
+
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
+}
+
+// create a SQL query as a string
+
+$sql_query = "SELECT * FROM marvelmovies";
+
+// execute the SQL query
+
+$result = $db->query($sql_query);
+
+// iterate over $result object one $row at a time
+// use fetch_array() to return an associative array
+
+while($row = $result->fetch_array()){
+   // print out fields from row of data
+   echo "<p>" . $row['title'] . "</p>";
+}
+
+$result->close();
+   // close connection to database
+   $db->close();
