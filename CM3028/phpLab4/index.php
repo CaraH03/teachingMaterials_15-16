8a4f0296593223b3e81669633740380660e03b7d
Mike Crabb <m.j.crabb@rgu.ac.uk>
2015-11-18 11:53:25 +0000

    asdf

---
+++ CM3028/phpLab4/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelcrabb
+ * Date: 18/11/2015
+ * Time: 11:37
+ */
+include ("db_connect.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<nav>
+<!-- Menu Creation -->
+    <?
+
+    $sql_query = "SELECT * FROM sectionText where topicID = '5'";
+    $result = $db->query($sql_query);
+    while($row = $result->fetch_array()){
+        echo "<li><a href='index.php?sectionID?'". $row['sectionID'].">".$row['sectionName']."</a>";
+    }
+?>
+
+
+</nav>
---
+++ CM3028/phpLab4/menustructure.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelcrabb
+ * Date: 18/11/2015
+ * Time: 11:37
+ */
+
