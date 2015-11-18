601c8083ed752045b05d9dd0da94061bd44b1297
Mike Crabb <m.j.crabb@rgu.ac.uk>
2015-11-18 10:17:56 +0000

    CM3028 Bits

---
+++ CM3028/phpLab2/finalTask/addInSuperhero.php
@@ -0,0 +1,15 @@
+<?
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$dob = $_POST["dob"];
+$gender = $_POST["gender"];
+$canfly = $_POST["canfly"];
+$zappy = $_POST["zapper"];
+$sarcasm = $_POST["sarcasm"];
+
+$sql = "INSERT INTO superherotask VALUES (firstName = '$firstname ', lastname = '$lastname', dob = '$dob', gender = '$gender', fly = '$canfly', zap = '$zappy', sarcastic = '$sarcasm')";
+$result = $db->query($sql);
+
+header('Location: index.html');
+
+?>
