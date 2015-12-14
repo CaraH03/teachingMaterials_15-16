23033afa32e95697a6e65cca052b0c3a9bfeaf64
Mike Crabb <m.j.crabb@rgu.ac.uk>
2015-12-14 12:04:44 +0000

    starter for 10

--- CM3028/sampleLoginSystem/checklogin.php
+++ CM3028/sampleLoginSystem/checklogin.php
@@ -19,10 +19,6 @@ $password = $_POST["password"];
 //$password = mysqli_real_escape_string($password);
 
 
-
-echo $username;
-echo $password;
-
 //FIND THE USER IN THE DATABASE
 $sql="SELECT * FROM users WHERE username='". $username ."' and password='". $password . "'";
 
@@ -39,22 +35,17 @@ while($row = $result->fetch_array()) {
     $loginSuccessful = 1;
 }
 
-
-
 // If result matched $username and $password, table row must be 1 row
 if($loginSuccessful==1){
 
-// Register $myusername, $mypassword and redirect to file "login_success.php"
-    echo "User Exists in Database";
-    /*
-    session_register("myusername");
-    session_register("mypassword");
-    header("location:login_success.php");
-    */
+// Register $myusername, $mypassword and redirect to file "index.php"
+    session_start();
+    $_SESSION['username'] = $username;
+    header("location:index.php");
+
 }
 else {
     echo "Wrong Username or Password";
-    echo "This changed again";
 }
 
 ?>
