85029b13c0a753ccbce31725e23c687db73522f9
Mike Crabb <m.j.crabb@rgu.ac.uk>
2015-12-14 12:53:34 +0000

    starter for 10

--- CM3028/sampleLoginSystem/index.php
+++ CM3028/sampleLoginSystem/index.php
@@ -8,10 +8,22 @@
 
 <?
 session_start();
+include ("db_connect.php");
+
 if (isset($_SESSION['username']))
 {
 //SESSION DOES EXIST
     echo "Hello " . $_SESSION['username'];
+
+    $sql = "SELECT * FROM users WHERE username='". $_SESSION['username'] . "'";
+
+    //RUN THE QUERY
+    $result = $db->query($sql);
+
+    while($row = $result->fetch_array())
+    {
+    echo "User type is " . $row['userType'];
+    }
     ?>
     <a href="logout.php">Logout</a>
     <?
