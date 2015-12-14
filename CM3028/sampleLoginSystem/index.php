b27462a83468d9bb790a67cc364ee68d09048c26
Mike Crabb <m.j.crabb@rgu.ac.uk>
2015-12-14 12:09:53 +0000

    starter for 10

---
+++ CM3028/sampleLoginSystem/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login Form</title>
+</head>
+<body>
+
+<?
+session_start();
+if ($_SESSION['username'] == "")
+{
+    //SESSION DOESNT EXIST
+    ?>
+    <h1>My Login Form</h1>
+    <form method="post" action="checklogin.php">
+        <p><input type="text" name="username" value="" placeholder="Username or Email"></p>
+        <p><input type="password" name="password" value="" placeholder="Password"></p>
+        <p class="submit"><input type="submit" name="commit" value="Login"></p>
+    </form>
+
+<?
+}
+else
+{
+    //SESSION DOES EXIST
+    echo "Hello " . $session['username'];
+}
+
+
+?>
+
+
+
+
+
+
+</body>
+</html>
