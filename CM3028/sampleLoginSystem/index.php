084164f0d785b26959c62c1055ba0d53cfa84de2
Mike Crabb <m.j.crabb@rgu.ac.uk>
2015-12-14 12:18:05 +0000

    starter for 10

--- CM3028/sampleLoginSystem/index.php
+++ CM3028/sampleLoginSystem/index.php
@@ -10,10 +10,11 @@
 session_start();
 if (isset($_SESSION['username']))
 {
-
-
-    //SESSION DOES EXIST
+//SESSION DOES EXIST
     echo "Hello " . $_SESSION['username'];
+    ?>
+    <a href="logout.php">Logout</a>
+    <?
 
 
 }
@@ -21,11 +22,13 @@ else
 {
 //SESSION DOESNT EXIST
 ?>
-<h1>My Login Form</h1>
+
+    <h1>My Login Form</h1>
 <form method="post" action="checklogin.php">
     <p><input type="text" name="username" value="" placeholder="Username or Email"></p>
     <p><input type="password" name="password" value="" placeholder="Password"></p>
     <p class="submit"><input type="submit" name="commit" value="Login"></p>
+
 </form>
 <?
 }
