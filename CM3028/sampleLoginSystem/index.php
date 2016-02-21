ab86cfa406e02c8c8858d077629241070e41027c
Mike Crabb <m.j.crabb@rgu.ac.uk>
2016-02-21 21:00:14 +0000

    0

--- CM3028/sampleLoginSystem/index.php
+++ CM3028/sampleLoginSystem/index.php
@@ -7,39 +7,40 @@
 <body>
 
 <?
+//Opens up a session
 session_start();
+
 include ("db_connect.php");
 
-if (isset($_SESSION['username']))
-{
-//SESSION DOES EXIST
-    echo "<p>Hello " . $_SESSION['username'] . "</p>";
+//Check to see if the 'username' session variable is set.
 
+if (isset($_SESSION['username'])) //SESSION DOES EXIST
+{
+
+    echo "<p>Hello " . $_SESSION['username'] . "</p>";
     $sql = "SELECT * FROM users WHERE username='". $_SESSION['username'] . "'";
     $result = $db->query($sql);
-
     while($row = $result->fetch_array())
     {
-    echo "<p>User type is " . $row['userType'] . "</p>";
+        echo "<p>User type is " . $row['userType'] . "</p>";
     }
-
     ?>
     <a href="logout.php">Logout</a>
     <?
 
 
 }
-else
+else //SESSION DOESNT EXIST
 {
-//SESSION DOESNT EXIST
+
 ?>
 
     <h1>My Login Form</h1>
-<form method="post" action="checklogin.php">
+    <form method="post" action="checklogin.php">
     <p><input type="text" name="username" value="" placeholder="Username or Email"></p>
     <p><input type="password" name="password" value="" placeholder="Password"></p>
     <p class="submit"><input type="submit" name="commit" value="Login"></p>
-</form>
+    </form>
     <a href="signup.php">Signup</a>
 
 <?
