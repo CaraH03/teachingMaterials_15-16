3e710e7191a732245354338fa5a850ab56300afb
Mike Crabb <m.j.crabb@rgu.ac.uk>
2016-03-18 11:40:49 +0000

    schoolWebsite

--- CMM007/LabMaterials/Week8/submitform.php
+++ CMM007/LabMaterials/Week8/submitform.php
@@ -1,8 +1,14 @@
 <?php
+//include database
+include ("db_connect.php");
 
 $firstname = $_POST["firstname"];
 $lastname = $_POST["lastname"];
 
+
 echo "Hello {$firstname} {$lastname}! How are you today?";
 
+$sql = "INSERT INTO newusers (firstname, lastname) VALUES ('{$firstname}', '{$lastname}')";
+
+header("location:viewusers.php");
 ?>
