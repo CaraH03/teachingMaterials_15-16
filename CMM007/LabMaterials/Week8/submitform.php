f8475bfb0185c02f53536aa8e1c5747b733767a8
Mike Crabb <m.j.crabb@rgu.ac.uk>
2016-03-18 11:43:32 +0000

    schoolWebsite

--- CMM007/LabMaterials/Week8/submitform.php
+++ CMM007/LabMaterials/Week8/submitform.php
@@ -7,5 +7,10 @@ $lastname = $_POST["lastname"];
 
 $sql = "INSERT INTO newusers (firstname, lastname) VALUES ('$firstname', '$lastname')";
 
+if (mysqli_query($db, $sql)) {
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($db);
+}
+
 //header("location:viewusers.php");
 ?>
