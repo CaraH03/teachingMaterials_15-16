0007a0c6883d2f3f88671891f9b1e7a11998d746
Mike Crabb <m.j.crabb@rgu.ac.uk>
2016-03-21 16:08:51 +0000

    0

--- CMM007/LabMaterials/Week8/superheroSystem/index.php
+++ CMM007/LabMaterials/Week8/superheroSystem/index.php
@@ -11,7 +11,7 @@
 </header>
 <p>What would you like to do?</p>
 <ul>
-    <li><a href="insertSuperhero.php">Insert a superhero</a></li>
+    <li><a href="superheroform.html">Insert a superhero</a></li>
     <li><a href="displaySuperheros.php">Display all superheros</a></li>
     <li><a href="battle.php">Insert a battle</a></li>
     <li><a href="displayBattles.php">Display all battles</a></li>
