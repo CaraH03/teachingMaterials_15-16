3f993804b8f2a269fc292418502f8341e79b1a89
Mike Crabb <m.j.crabb@rgu.ac.uk>
2016-04-12 14:05:44 +0100

    bug1

--- CMM503/sqlInjection/bugs.php
+++ CMM503/sqlInjection/bugs.php
@@ -3,6 +3,8 @@ include('db_connect.php');
 $bugID = $_GET['bugID'];
 $sql_query = "SELECT * FROM softwareBugs WHERE bugID = '".$bugID."'";
 
+$result = $db->query($sql_query);
+
 while($row = $result->fetch_array())
 {
     echo "<h4>Bug Name: {$row['bugName']}</h4>";
